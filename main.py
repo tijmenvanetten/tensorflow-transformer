32972baa07343339682a7020e95f81de008395d9
tijmenvanetten <33256845+tijmenvanetten@users.noreply.github.com>
2021-03-08 12:31:51 +0100

    Finished Attention Translater Notebook

---
+++ main.py
@@ -0,0 +1,28 @@
+import collections
+import logging
+import os
+import pathlib
+import re
+import string
+import sys
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow_datasets as tfds
+import tensorflow_text as text
+import tensorflow as tf
+
+logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
+
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+train_examples, val_examples = examples['train'], examples['validation']
+
+model_name = "ted_hrlr_translate_pt_en_converter"
+tf.keras.utils.get_file(
+    f"{model_name}.zip",
+    f"https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip",
+    cache_dir='.', cache_subdir='', extract=True
+)
